33d530e99a69bdef29459f762e4530e7c2272129
kklee0930 <kklee0930@gmail.com>
2024-01-03 19:27:41 +0900

    setting

---
+++ src/main/java/com/example/tubePlusM/TubePlusMApplication.java
@@ -0,0 +1,13 @@
+package com.example.tubePlusM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubePlusMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubePlusMApplication.class, args);
+	}
+
+}
